402a4ca43d551039f8f3d020e21905654b508242
t_klimova@inbox.ru <t_klimova@inbox.ru>
2022-10-05 16:28:34 +0300

    corrected Task1 and added Task2

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -1,4 +1,6 @@
 
+import com.codeborne.selenide.Selectors;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
@@ -6,28 +8,51 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 
 public class CardDeliveryTest {
+    String GenerateDate(int daysPlus, String pattern) {
+        return LocalDate.now().plusDays(daysPlus).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:9999");
+    }
 
     @Test
     void shouldBookMeeting() {
-        open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Казань");
-        $("[data-test-id='date'] .input__control").doubleClick();
-        $("[data-test-id='date'] .input__control").sendKeys(Keys.BACK_SPACE);
-//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-//        LocalDate date = LocalDate.now().plusDays(3);
-//        String dataString = date.format(formatter);
-        $("[data-test-id='date'] .input__control").setValue(LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+        $("[data-test-id='date'] .input__control").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME, Keys.BACK_SPACE));
+        $("[data-test-id='date'] .input__control").setValue(GenerateDate(3, "dd.MM.yyyy"));
         $("[data-test-id='name'] input").setValue("Тестовый Случай");
         $("[data-test-id='phone'] input").setValue("+79200000000");
         $("[data-test-id='agreement']").click();
         $("button.button").click();
         $(byText("Встреча успешно забронирована на")).shouldBe(visible, Duration.ofSeconds(15));
+        $(byText(GenerateDate(3, "dd.MM.yyyy"))).shouldBe(visible);
     }
 
+    @Test
+    void shouldBookMeetingWithComplexElements() {
+        $("[data-test-id='city'] input").setValue("вл");
+        $(byText("Владивосток")).click();
+        $(".input__icon").click();
+        LocalDate dateDefault = LocalDate.now();
+        LocalDate dateMeeting = LocalDate.now().plusDays(7);
+        if (dateMeeting.getMonthValue() > dateDefault.getMonthValue() | dateMeeting.getYear() > dateDefault.getYear()) {
+            $(".calendar__arrow_direction_right[data-step='1']").click();
+        }
+        $$("td.calendar__day").find(exactText(GenerateDate(7, "d"))).click();
+        $("[data-test-id='name'] input").setValue("Тестовый Случай");
+        $("[data-test-id='phone'] input").setValue("+79200000000");
+        $("[data-test-id='agreement']").click();
+        $("button.button").click();
+        $(byText("Встреча успешно забронирована на")).shouldBe(visible, Duration.ofSeconds(15));
+        $(byText(GenerateDate(7, "dd.MM.yyyy"))).shouldBe(visible);
+    }
 }
